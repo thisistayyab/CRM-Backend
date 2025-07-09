879fba312f48611e3387a4cf1486a7f85592de2b
Tayyab Aslam <thisistayyab@outlook.com>
2025-07-09 15:54:17 +0500

    Update: Company Name

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(cors({
         if (!origin) return callback(null, true);
         
         const allowedOrigins = [
-            "https://crm-frontend-tawny-nine.vercel.app",
+            "https://taylancecrm.vercel.app",
             "http://localhost:5173",
             "http://localhost:3000"
         ];
